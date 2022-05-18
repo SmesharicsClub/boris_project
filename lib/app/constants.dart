4fdd8e24feee180fccbd153fe96c18ca5ea271eb
B-andD <ddboriskin@gmail.com>
2022-05-18 17:06:34 +0300

    fix linter warning and create new files par two

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -5,4 +5,5 @@ class Constants {
   static const Color backgroundColor = Color(0xFF121212);
   static const Color thirdContainerColor = Color(0x4FFD0000);
   static const String containerText = 'Скажи-ка, дядя, ведь не даром...';
+  static const String printPath = 'assets/images/image.png';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app/constants.dart';
-import 'app/imageWidjet.dart';
+import 'app/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
